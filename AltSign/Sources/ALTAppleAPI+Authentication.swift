db120fcfc499441538b119d544d3d52e448e6cf2
Riley Testut <riley@rileytestut.com>
2023-01-19 12:54:57 -0600

    Throws ALTAppleAPIErrorIncorrectCredentials when Apple ID email doesn’t exist
    -22406 is thrown when the password is incorrect, but -20101 is thrown when the provided email is incorrect.

--- AltSign/Sources/ALTAppleAPI+Authentication.swift
+++ AltSign/Sources/ALTAppleAPI+Authentication.swift
@@ -461,7 +461,7 @@ private extension ALTAppleAPI
                     
                     switch errorCode
                     {
-                    case -22406: throw ALTAppleAPIError(.incorrectCredentials)
+                    case -20101, -22406: throw ALTAppleAPIError(.incorrectCredentials)
                     case -22421: throw ALTAppleAPIError(.invalidAnisetteData)
                     default:
                         guard let errorDescription = status["em"] as? String else { throw ALTAppleAPIError.unknown() }
